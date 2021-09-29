00de86245e6369e60fdd5e67f0e9e59f03cf671b
Leon Bowie <leon@bowie-co.nz>
2021-09-30 09:27:16 +1300

    merge changes

--- defectio/__init__.py
+++ defectio/__init__.py
@@ -13,29 +13,16 @@ __title__ = "defectio"
 __author__ = "Darkflame72"
 __license__ = "MIT"
 __copyright__ = "Copyright 2021-present Darkflame72"
-__version__ = "0.2.1a"
+__version__ = "0.3.0"
 
 __path__ = __import__("pkgutil").extend_path(__path__, __name__)
 
 import logging
 from typing import NamedTuple, Literal
 
-from defectio.client import Client
-
 # expose all models at the top level
-from defectio.models.apiinfo import *
-from defectio.models.attachment import *
-from defectio.models.auth import *
-from defectio.models.channel import *
-from defectio.models.colour import *
-from defectio.models.file import *
-from defectio.models.member import *
-from defectio.models.message import *
-from defectio.models.objects import *
-from defectio.models.permission import *
-from defectio.models.server import *
-from defectio.models.user import *
-from defectio.models.raw_models import *
+from defectio.models import *
+from defectio.api.bot import Client
 
 __all__ = (
     "__title__",
@@ -57,7 +44,7 @@ class VersionInfo(NamedTuple):
 
 
 version_info: VersionInfo = VersionInfo(
-    major=0, minor=2, micro=1, releaselevel="alpha", serial=0
+    major=0, minor=3, micro=0, releaselevel="alpha", serial=0
 )
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
